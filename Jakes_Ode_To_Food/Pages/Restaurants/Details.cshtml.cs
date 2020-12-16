d98cbcee5d081965ed4b335c8f5c3cdd61d138ca
Jake Mander <32242126+JakeMander@users.noreply.github.com>
2020-12-16 15:50:38 +0000

    Completed Sections: Building A Detail Page And Linking To The Details.

---
+++ Jakes_Ode_To_Food/Pages/Restaurants/Details.cshtml.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Core;
+
+namespace Jakes_Ode_To_Food.Pages.Restaurants
+{
+    public class DetailsModel : PageModel
+    {
+        public Restaurant Restaurant { get; set; }   
+        public void OnGet(int restaurauntId)
+        {
+            Restaurant = new Restaurant();
+            Restaurant.Id = restaurauntId;
+        }
+    }
+}
